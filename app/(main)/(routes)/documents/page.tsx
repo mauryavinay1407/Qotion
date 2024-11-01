413f54e28e93ea93e0d7e461a8bed92bcc2bf87d
mauryavinay1407 <vinaymaurya1407@gmail.com>
2024-11-01 23:29:16 +0530

    Docs page added

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,8 +1,37 @@
+"use client";
+
+import { Button } from '@/components/ui/button';
+import { useUser } from '@clerk/clerk-react';
+import { PlusCircle } from 'lucide-react';
+import Image from 'next/image';
 import React from 'react'
 
 const DocumentsPage = () => {
+  const {user} = useUser(); 
   return (
-    <div>This is docs</div>
+    <div className='h-full flex flex-col items-center justify-center space-y-4'>
+    <Image
+     src={"/empty.png"}
+     height={"300"}
+     width={"300"}
+     alt='Empty'
+     className='dark:hidden'
+    />
+    <Image
+     src={"/empty-dark.png"}
+     height={"300"}
+     width={"300"}
+     alt='Empty'
+     className='hidden dark:block'
+    />
+    <h2>
+      Welcome to {user?.firstName}&apos;s Qotion
+    </h2>
+    <Button >
+      <PlusCircle className='h-4 w-4 mr-2'/>
+      Create note
+    </Button>
+    </div>
   )
 }
 
