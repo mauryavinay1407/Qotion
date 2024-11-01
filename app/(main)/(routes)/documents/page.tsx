58ab56ce4cae46d6e24a013d50b046546fe6cdda
mauryavinay1407 <vinaymaurya1407@gmail.com>
2024-11-02 03:06:25 +0530

    feat:Schema and api added

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -2,12 +2,28 @@
 
 import { Button } from '@/components/ui/button';
 import { useUser } from '@clerk/clerk-react';
+import { useMutation } from 'convex/react';
 import { PlusCircle } from 'lucide-react';
 import Image from 'next/image';
 import React from 'react'
 
+import {api} from "@/convex/_generated/api";
+import {toast} from "sonner";
+
 const DocumentsPage = () => {
+  const create = useMutation(api.documents.create);
   const {user} = useUser(); 
+
+  const onCreate = () =>{
+  const promise = create({title: "Untitled"});
+
+  toast.promise(promise,{
+    loading: " Creating a new note...",
+    success: "New note created!",
+    error: "Failed to create a new note"
+  })
+  }
+
   return (
     <div className='h-full flex flex-col items-center justify-center space-y-4'>
     <Image
@@ -27,7 +43,7 @@ const DocumentsPage = () => {
     <h2>
       Welcome to {user?.firstName}&apos;s Qotion
     </h2>
-    <Button >
+    <Button onClick={onCreate}>
       <PlusCircle className='h-4 w-4 mr-2'/>
       Create note
     </Button>
