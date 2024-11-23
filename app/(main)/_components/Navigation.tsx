b5064377c83192353d00399da235ce71c96a2477
mauryavinay1407 <vinaymaurya1407@gmail.com>
2024-11-23 21:49:32 +0530

    Search functionality added

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -11,12 +11,13 @@ import { toast } from "sonner";
 import { DocumentList } from "./document-list";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import TrashBox from "./trash-box";
+import { useSearch } from "@/hooks/user-search";
 
 const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create);  
-
+  const search = useSearch();
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -136,7 +137,7 @@ const Navigation = () => {
              label="Search"
              icon={Search}
              isSearch
-             onClick={()=>{}}
+             onClick={search.onOpen}
             />
             <Item
              label="Settings"
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,7 +4,7 @@ import { Spinner } from "@/components/Spinner";
 import { useConvexAuth } from "convex/react"
 import { redirect } from "next/navigation";
 import Navigation from "./_components/Navigation";
-
+import { SearchCommand } from "@/components/search-command";
 
 export default function MainLayout ({
     children 
@@ -28,6 +28,7 @@ export default function MainLayout ({
         <div className="h-full flex dark:bg-[#1F1F1F]">
             <Navigation/>
             <main className="flex-1 h-full overflow-y-auto">
+                <SearchCommand/>
                 {children}
             </main>
         </div>
